7eadf57b45214036d6dc65dfff77db5aa448d9d9
Mohan Dodda <mohandodda@rocketmail.com>
2022-03-10 17:49:56 -0500

    change

--- src/components/PrevRated.js
+++ src/components/PrevRated.js
@@ -29,52 +29,73 @@ function PrevRated() {
   let [allSummaries, setAllSummaries] = useState([])
   let [allDialogueLines, setAllDialogueLines] = useState([])
   let [page, setPage] = useState(1)
+  let [summaryPairs, setSummaryPairs] = useState([])
+  let [allScores, setAllScores] = useState([])
+  const summarymodels = ['Salesforce/bart-large-xsum-samsum', 'philschmid/distilbart-cnn-12-6-samsum', 'henryu-lin/t5-large-samsum-deepspeed', 'linydub/bart-large-samsum', 'knkarthick/meeting-summary-samsum']
+  let vs = ['Salesforce/bart-large-xsum-samsum vs linydub/bart-large-samsum', 'Salesforce/bart-large-xsum-samsum vs linydub/bart-large-samsum', 'philschmid/distilbart-cnn-12-6-samsum vs knkarthick/meeting-summary-samsum']
+  const testannotations = [52, 53, 54, 55, 56, 57, 58, 59, 60, 61]
 
-  
   const getDocuments = async () => {
-    const stateQuery = query(collection(db, "responses"), where("name", "==", localStorage.getItem('name')));
-    const querySnapshot = await getDocs(stateQuery);
-    // console.log(querySnapshot)
+    // const stateQuery = query(collection(db, "responses"), where("name", "==", localStorage.getItem('name')));
+    // const querySnapshot = await getDocs(stateQuery);
+    let localname = localStorage.getItem('name')
+    const annotatorRef = doc(db, 'annotators2', localname);
+    console.log(annotatorRef)
+    let annotatorDoc = await getDoc(annotatorRef)
     let d = []
-    querySnapshot.forEach((doc) => {
-        // console.log(doc.data())
-        // console.log(doc.data().salientInfoAll)
-        // doc.data() is never undefined for query doc snapshots
+    annotatorDoc.data().testannotations.forEach((doc) => {
         d.push(doc.data())
       });
     console.log('lasty', d)
     setAllData(d)
     let dialogueLinesAll = []
     let smmrys = []
+    let texts = JSON.parse(JSON.stringify(jsonData))
+    let out = [[0,3], [1,4], [3,4]]
+    let scores = []
+    setSummaryPairs(out)
     for (let i = 0; i < d.length; i++) {
       const docSnap = await getDoc(d[i].summary);
       let smmrydata = docSnap.data()
-      if (smmrydata) {
-        smmrys.push(smmrydata.summary)
-        let dialogueLines = []
-        let salientInfoAllOne = [...d[i].salientInfoAll].reverse()
-        let lines = smmrydata.dialogue.split("\n")
-        for (let j = 0; j < lines.length; j++) {
-          let line = [lines[j]]
-          // dialogueLines.push(lines[j])
-          while (salientInfoAllOne.length > 0 && salientInfoAllOne[salientInfoAllOne.length-1].lineno==j) {
-            line.push(salientInfoAllOne.pop())
-          }
-          dialogueLines.push(line)
+      let arr = smmrydata.fname.split("_")
+      let fname = arr[arr.length-1]
+      console.log(fname)
+      let text = texts[parseInt(fname)]
+      let sm = []
+      scores.push(d[i].scores)
+
+      out.forEach((pair) => {
+        let first = text['summary'+summarymodels[pair[0]]]
+        let second = text['summary'+summarymodels[pair[1]]]
+        let smmry = [first, second]
+        sm.push(smmry)
+        // let key = summarymodels[pair[0]]+' vs ' +summarymodels[pair[1]]
+        // let smmry = text[first]
+        // if (!(key in currScores)) {
+        //   currScores[key] = {'Coherence':0, 'Accuracy':0, 'Coverage':0, 'Concise':0, 'Overall Quality':0}
+        // }
+      });
+      smmrys.push(sm)
+
+      let salientInfoAllOne = [...d[i].salientInfoAll].reverse()
+      let lines = text.dialogue.split("\n")
+      let dialogueLines = []
+      for (let j = 0; j < lines.length; j++) {
+        let line = [lines[j]]
+        // dialogueLines.push(lines[j])
+        while (salientInfoAllOne.length > 0 && salientInfoAllOne[salientInfoAllOne.length-1].lineno==j) {
+          line.push(salientInfoAllOne.pop())
         }
-        dialogueLinesAll.push(dialogueLines)
+        dialogueLines.push(line)
       }
+      dialogueLinesAll.push(dialogueLines)
     }
+    console.log(dialogueLinesAll)
     setAllDialogueLines(dialogueLinesAll)
     setAllSummaries(smmrys)
-    // console.log('ehrererere')
-    // console.log(d, smmrys, dialogueLinesAll)
-    // console.log(d)
-    // console.log(smmrys)
-    // console.log(dialogueLinesAll.length)
-    // console.log(dialogueLinesAll[0])
+    setAllScores(scores)
+    console.log(smmrys, dialogueLinesAll, d)
    };
-
 
   useEffect(() => {
     if (localStorage.getItem('name') && localStorage.getItem('name')!="") {
@@ -87,7 +108,6 @@ function PrevRated() {
   }, []);
 
   let criterias = ['Coherence', 'Accuracy', 'Coverage', 'Concise', 'Overall Quality']
-//   let criteriaChangeFunctions = {'Coherence':callbackSetCoherence, 'Accuracy':callbackSetAccuracy, 'Coverage':callbackSetCoverage, 'Concise':callbackSetConcise, 'Overall Quality':callbackSetOverall}
   
   let marks= {
       "-2": ' A mostly better',
@@ -139,7 +159,7 @@ function PrevRated() {
             ranges={line.slice(1,line.length)}
             enabled={false}
             onTextHighlighted={onTextHighlightedCallback}
-            id={1}
+            id={"1"}
             onMouseOverHighlightedWord={onMouseOverHighlightedWordCallback}
             highlightStyle = {{
               backgroundColor: color
@@ -153,8 +173,8 @@ function PrevRated() {
         </div> 
         : <></>
       }
-        <div className="finalbutton"> 
-         {  (allSummaries && allSummaries[0] && allSummaries[0].length>1)  ? 
+        {/* <div className="finalbutton"> 
+         { (allSummaries && allSummaries[0] && allSummaries[0].length>1)  ? 
         <div className="Summaries" >
           <div className="SummaryVal">
             <div><h2>Summary A</h2></div>
@@ -164,7 +184,7 @@ function PrevRated() {
             {allSummaries[page-1][1]}
             </div>
         </div>
-        : <></>}  
+        : <></>}
         {criterias.map((criteria) => (
                       <div className="ratings">
                         <div className="ratingstext">
@@ -173,7 +193,43 @@ function PrevRated() {
                       <Slider  min={-2} max={2} marks={marks} step={null}  value={allData[page-1].scores[criteria] || 0} disabled={true}/>
                       </div>
               ))}
-      </div>
+      </div> */}
+
+
+      {
+         summaryPairs.map((pair, index) =>
+
+         <div className="finalbutton"> 
+
+         <div className="Summaries" >
+           <div className="SummaryVal">
+             <div><h2>Summary A</h2></div>
+               {allSummaries[page-1][index][0]
+              //  document['summary'+summarymodels[pair[0]]]
+               }
+             </div>
+           
+           <div className="SummaryVal">
+             <div><h2>Summary B</h2></div>
+               {
+                 allSummaries[page-1][index][1]
+              //  document['summary'+summarymodels[pair[1]]]
+               }
+             </div>
+         </div>
+         {criterias.map((criteria) => (
+                       <div className="ratings">
+                         <div className="ratingstext">
+                             <h3>Please compare the two above summaries in regards to their {criteria}</h3>
+                         </div>
+                         <Slider  min={-2} max={2} marks={marks} step={null}  value={allScores[page-1][vs[index]][criteria]   || 0} disabled={true}/>
+                         {/* <Slider  min={-2} max={2} marks={marks} step={null} onChange={(value) => setScore(value, criteria, pair)} defaultValue={0} /> */}
+                       {/* <Slider  min={-2} max={2} marks={marks} step={null} onChange={criteriaChangeFunctions[criteria]} defaultValue={0} /> */}
+                       </div>
+               ))}
+       </div>
+         )
+       }
 
       <Pagination simple onChange={onChange} defaultCurrent={1} total={allDialogueLines.length} pageSize={1} />
 
